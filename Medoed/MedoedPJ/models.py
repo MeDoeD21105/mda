836feabc1c8b616358d98d6633835f796b6ff17f
MeDoeD です <medoed21105@gmail.com>
2024-01-14 14:27:18 +0300

    modif model

--- Medoed/MedoedPJ/models.py
+++ Medoed/MedoedPJ/models.py
@@ -9,3 +9,4 @@ class Product(models.Model):
     title = models.CharField(max_length = 100)
     content = models.TextField()
     price = models.DecimalField( max_digits=7, decimal_places=2)
+    photo = models.ImageField(upload_to='media', null=True)
