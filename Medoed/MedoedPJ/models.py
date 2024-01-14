d264db2644607506a5d61fd0d984e26e2099279a
MeDoeD です <medoed21105@gmail.com>
2024-01-14 13:54:32 +0300

    added models

--- Medoed/MedoedPJ/models.py
+++ Medoed/MedoedPJ/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+
+from django.db import models
+
+# Create your models here.
+class Product(models.Model):
+    title = models.CharField(max_length = 100)
+    content = models.TextField()
+    price = models.DecimalField( max_digits=7, decimal_places=2)
