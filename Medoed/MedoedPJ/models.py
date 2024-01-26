285a2cc45095a83eb14c49d47ee3849678540cf2
MeDoeD です <medoed21105@gmail.com>
2024-01-26 15:09:01 +0300

    added models

--- Medoed/MedoedPJ/models.py
+++ Medoed/MedoedPJ/models.py
@@ -1,12 +1,12 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
 
 from django.db import models
 
 # Create your models here.
-class Product(models.Model):
-    title = models.CharField(max_length = 100)
+class Post (models.Model):
+    title = models.CharField(max_length = 200)
     content = models.TextField()
-    price = models.DecimalField( max_digits=7, decimal_places=2)
-    photo = models.ImageField(upload_to='media', null=True)
+    create_data = models.DateTimeField(auto_now_add=True)
+    author = models.ForeignKey(User, on_delete = models.CASCADE)
