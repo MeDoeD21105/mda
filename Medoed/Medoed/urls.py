1af901006c91a658c9440d2688ed6f475602a9df
MeDoeD です <medoed21105@gmail.com>
2024-02-01 01:19:27 +0300

    modif models photo

--- Medoed/Medoed/urls.py
+++ Medoed/Medoed/urls.py
@@ -21,8 +21,8 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path("",include("MedoedPJ.urls"))
-] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    path('', include('MedoedPJ.urls')),
+]
 
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
